578186f40e2be36ac90593e2bafbdc4b6f1a59dd
github username <github email address>
2018-03-07 17:09:45 -0500

    seeded with photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,11 @@ cities_list_arr = cities_hash["_links"]["ua:item"]
 cities_list_arr.each do |city|
   Place.create(city: city["name"])
 end
+
+
+cities_list_arr.each do |city|
+  photos_unparsed = RestClient.get("#{city['href']}images")
+  photos_hash = JSON.parse(photos_unparsed)
+  img_url = photos_hash["photos"][0]["image"]["web"]
+  Photo.create(url: img_url, place_id: Place.find_by(city: city["name"]).id)
+end
