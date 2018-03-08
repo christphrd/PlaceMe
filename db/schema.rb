13a4c7a9a32bfe7ef81347801f6b8b3cdf7d9669
allinthemind66 <r.nilon92@gmail.com>
2018-03-08 17:14:57 -0500

    added continents and other such stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180307213150) do
+ActiveRecord::Schema.define(version: 20180308201845) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20180307213150) do
     t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "continent"
   end
 
   create_table "user_places", force: :cascade do |t|
