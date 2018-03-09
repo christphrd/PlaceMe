8894175a935648b7e404e3ab5a2323a478263ef3
github username <github email address>
2018-03-09 01:18:09 -0500

    got search to work for country and continent

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   post '/places/:id', to: 'places#update'
   # get '/users/signup', to: 'users#new'
   # post '/places/:id', to: 'comments#new'
+  get '/search', to: 'search#search'
 end
